c606ac2939c312c3430ea96d6890af02c6490df1
GOPINATH <ngopi6769@gmail.com>
2025-07-30 15:28:04 +0530

    commit

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request,jsonify,render_template
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -15,7 +15,7 @@ standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods = ['GET','POST'])
+@app.route('/predictdata', methods = ['GET','POST'])
 
 def predict_datapoint():
     if request.method == "POST":
@@ -23,7 +23,7 @@ def predict_datapoint():
         RH = float(request.form.get('RH'))
         Ws = float(request.form.get('Ws'))
         Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC '))
+        FFMC = float(request.form.get('FFMC'))
         DMC = float(request.form.get('DMC'))
         ISI = float(request.form.get('ISI'))
         Classes = float(request.form.get('Classes'))
@@ -32,11 +32,11 @@ def predict_datapoint():
         new_data_scaled =standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
         result = ridge_model.predict(new_data_scaled)
 
-        return render_template('home.html',results=result[0])
+        return render_template('home.html', results=round(result[0],2))
     else:
         return render_template('home.html')
 
 
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
